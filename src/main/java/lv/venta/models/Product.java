de1597127aac3ac807d22172c748b37638b4c147
localuser <localuser@VEA.LV>
2023-05-11 11:36:23 +0300

    New: add validation in model class

--- src/main/java/lv/venta/models/Product.java
+++ src/main/java/lv/venta/models/Product.java
@@ -3,13 +3,36 @@ package lv.venta.models;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class Product {
 
 	private int id;
+	
+	@NotNull
+	@Pattern(regexp = "[A-ZĒŪĪĻĶŠĀŽČŅ]{1}[a-zēūīļķšāžčņ\\ ]+")
+	@Size(min = 3, max = 30)
 	private String title;
+	
+	@Min(value = 0)
+	@Max(value = 10000)
 	private float price;
+	
+	@NotNull
+	@Pattern(regexp = "[A-ZĒŪĪĻĶŠĀŽČŅ]{1}[a-zēūīļķšāžčņ\\ ]+")
+	@Size(min = 3, max = 100)
 	private String description;
+	
+	
+	@Min(value = 0)
+	@Max(value = 1000)
 	private int quantity;
+	
+	
 	private static int idCounter = 0;
 	
 	public int getId() {
